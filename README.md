76395a39d10e9dd975387d15cc7f312328397550
tyfiero <77432183+tyfiero@users.noreply.github.com>
2023-06-05 15:55:20 -0700

    Update README

--- README.md
+++ README.md
@@ -1,1 +1,39 @@
-# Satirical Silicon
+# Satirical Silicon: The Absurdist Odyssey of AI-Generated Tech News 🤖💻
+
+Ahoy, code wrangler! You've stumbled onto the grand spectacle of Satirical Silicon - a beacon of chaos in the orderly world of tech. It's a place where programming, parody, and an AI with a sense of humor rivalling that of a dial-up modem mingle to birth perfect(ly preposterous) prose.
+
+## What is this madness, you ask? 🤔
+
+Satirical Silicon is an open-source project that uses cutting-edge artificial intelligence to automatically post a blog every day. And not just any blog, but a satirical take on tech news. Imagine your tech feed after a weekend bender in Vegas, and you're halfway there.
+
+## Tech Stack 🏭
+
+This unholy matrimony of tech nonsense wouldn't be possible without a stack that sounds like a hipster's grocery list:
+
+- React: Because who doesn't love spending hours on end debugging?
+- Next.js: When you want to go serverless, but still want to feel like you're doing something important.
+- MDX: Because Markdown was too easy.
+- Langchain: A tool for creating AI chains. Or is it a chain for creating AI tools? Who knows.
+- Tailwind CSS: A utility-first CSS framework for rapidly building custom designs. Or rapidly tearing out your hair. Depends on the day.
+
+## How Do You Use It? 🤷‍♀️
+
+It's as easy as falling off a log! Actually, that sounds painful. Let's say it's as easy as eating pie. Unless you're on a diet. Nevermind, here are the steps:
+
+1. Fork this repository, because who doesn't like some good old fashioned cutlery.
+2. Clone it to your local machine. Or quantum computer. We're not here to judge.
+3. Install the dependencies using `npm install`. This might be a good time to make a cup of coffee.
+4. Run the project using `npm run dev`.
+5. Delete the posts in the posts directory and add your own, if you want. Or just steal mine, that's cool too.
+6. Enjoy the satirical posts! Or, you know, weep for the future of humanity. Again, no judgement.
+
+## Contributing: Because Misery Loves Company 👫
+
+Fancy adding your own two bytes to this masterpiece? Go ahead, fork this repository, add your magic (or tragic - we appreciate both), and then submit a pull request. We promise to review it as soon as we've deciphered the latest AI-generated post.
+
+## Disclaimer ⚠️
+
+We're not responsible for any laughter, tears, or existential crises that may result from using this project.
+
+Happy coding, or whatever it is you people do!
+
--- components/etc/Accordion.tsx
+++ components/etc/Accordion.tsx
@@ -1,177 +1,220 @@
-export default function Accordion() {
+    export default function Accordion() {
     return (
-      <div className="hs-accordion-group">
-        <div className="hs-accordion active" id="hs-basic-heading-one">
-          <button
-            className="inline-flex items-center w-full py-3 font-semibold text-left text-gray-800 transition hs-accordion-toggle hs-accordion-active:text-blue-600 group gap-x-3 hover:text-gray-500 dark:hs-accordion-active:text-blue-500 dark:text-gray-200 dark:hover:text-gray-400"
-            aria-controls="hs-basic-collapse-one"
-          >
-            <svg
-              className="block w-3 h-3 text-gray-600 hs-accordion-active:hidden hs-accordion-active:text-blue-600 hs-accordion-active:group-hover:text-blue-600 group-hover:text-gray-500 dark:text-gray-400"
-              width="16"
-              height="16"
-              viewBox="0 0 16 16"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                d="M2.62421 7.86L13.6242 7.85999"
-                stroke="currentColor"
-                strokeWidth="2"
-                strokeLinecap="round"
-              />
-              <path
-                d="M8.12421 13.36V2.35999"
-                stroke="currentColor"
-                strokeWidth="2"
-                strokeLinecap="round"
-              />
-            </svg>
-            <svg
-              className="hidden w-3 h-3 text-gray-600 hs-accordion-active:block hs-accordion-active:text-blue-600 hs-accordion-active:group-hover:text-blue-600 group-hover:text-gray-500 dark:text-gray-400"
-              width="16"
-              height="16"
-              viewBox="0 0 16 16"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                d="M2.62421 7.86L13.6242 7.85999"
-                stroke="currentColor"
-                strokeWidth="2"
-                strokeLinecap="round"
-              />
-            </svg>
-            Accordion #1
-          </button>
-          <div
-            id="hs-basic-collapse-one"
-            className="hs-accordion-content w-full overflow-hidden transition-[height] duration-300"
-            aria-labelledby="hs-basic-heading-one"
-          >
-            <p className="text-gray-800 dark:text-gray-200">
-              <em>This is the third item's accordion body.</em> It is hidden by
-              default, until the collapse plugin adds the appropriate classes that
-              we use to style each element. These classes control the overall
-              appearance, as well as the showing and hiding via CSS transitions.
-            </p>
-          </div>
+    <div className="hs-accordion-group">
+    <div className="hs-accordion active" id="hs-basic-heading-one">
+        <button
+        className="inline-flex items-center w-full py-3 font-semibold text-left text-gray-800 transition hs-accordion-toggle hs-accordion-active:text-blue-600 group gap-x-3 hover:text-gray-500 dark:hs-accordion-active:text-blue-500 dark:text-gray-200 dark:hover:text-gray-400"
+        aria-controls="hs-basic-collapse-one"
+        >
+        <svg
+            className="block w-3 h-3 text-gray-600 hs-accordion-active:hidden hs-accordion-active:text-blue-600 hs-accordion-active:group-hover:text-blue-600 group-hover:text-gray-500 dark:text-gray-400"
+            width="16"
+            height="16"
+            viewBox="0 0 16 16"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+        >
+            <path
+            d="M2.62421 7.86L13.6242 7.85999"
+            stroke="currentColor"
+            strokeWidth="2"
+            strokeLinecap="round"
+            />
+            <path
+            d="M8.12421 13.36V2.35999"
+            stroke="currentColor"
+            strokeWidth="2"
+            strokeLinecap="round"
+            />
+        </svg>
+        <svg
+            className="hidden w-3 h-3 text-gray-600 hs-accordion-active:block hs-accordion-active:text-blue-600 hs-accordion-active:group-hover:text-blue-600 group-hover:text-gray-500 dark:text-gray-400"
+            width="16"
+            height="16"
+            viewBox="0 0 16 16"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+        >
+            <path
+            d="M2.62421 7.86L13.6242 7.85999"
+            stroke="currentColor"
+            strokeWidth="2"
+            strokeLinecap="round"
+            />
+        </svg>
+        But like, why? WTF is the point?
+        </button>
+        <div
+        id="hs-basic-collapse-one"
+        className="hs-accordion-content w-full overflow-hidden transition-[height] duration-300"
+        aria-labelledby="hs-basic-heading-one"
+        >
+        <p className="text-gray-800 dark:text-gray-200">
+            Why not! No lie though, I asked myself this very question many times while making it.
+        </p>
         </div>
-  
-        <div className="hs-accordion" id="hs-basic-heading-two">
-          <button
-            className="inline-flex items-center w-full py-3 font-semibold text-left text-gray-800 transition hs-accordion-toggle hs-accordion-active:text-blue-600 group gap-x-3 hover:text-gray-500 dark:hs-accordion-active:text-blue-500 dark:text-gray-200 dark:hover:text-gray-400"
-            aria-controls="hs-basic-collapse-two"
-          >
-            <svg
-              className="block w-3 h-3 text-gray-600 hs-accordion-active:hidden hs-accordion-active:text-blue-600 hs-accordion-active:group-hover:text-blue-600 group-hover:text-gray-500 dark:text-gray-400"
-              width="16"
-              height="16"
-              viewBox="0 0 16 16"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                d="M2.62421 7.86L13.6242 7.85999"
-                stroke="currentColor"
-                strokeWidth="2"
-                strokeLinecap="round"
-              />
-              <path
-                d="M8.12421 13.36V2.35999"
-                stroke="currentColor"
-                strokeWidth="2"
-                strokeLinecap="round"
-              />
-            </svg>
-            <svg
-              className="hidden w-3 h-3 text-gray-600 hs-accordion-active:block hs-accordion-active:text-blue-600 hs-accordion-active:group-hover:text-blue-600 group-hover:text-gray-500 dark:text-gray-400"
-              width="16"
-              height="16"
-              viewBox="0 0 16 16"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                d="M2.62421 7.86L13.6242 7.85999"
-                stroke="currentColor"
-                strokeWidth="2"
-                strokeLinecap="round"
-              />
-            </svg>
-            Accordion #2
-          </button>
-          <div
-            id="hs-basic-collapse-two"
-            className="hs-accordion-content hidden w-full overflow-hidden transition-[height] duration-300"
-            aria-labelledby="hs-basic-heading-two"
-          >
-            <p className="text-gray-800 dark:text-gray-200">
-              <em>This is the third item's accordion body.</em> It is hidden by
-              default, until the collapse plugin adds the appropriate classes that
-              we use to style each element. These classes control the overall
-              appearance, as well as the showing and hiding via CSS transitions.
-            </p>
-          </div>
+    </div>
+
+    <div className="hs-accordion" id="hs-basic-heading-two">
+        <button
+        className="inline-flex items-center w-full py-3 font-semibold text-left text-gray-800 transition hs-accordion-toggle hs-accordion-active:text-blue-600 group gap-x-3 hover:text-gray-500 dark:hs-accordion-active:text-blue-500 dark:text-gray-200 dark:hover:text-gray-400"
+        aria-controls="hs-basic-collapse-two"
+        >
+        <svg
+            className="block w-3 h-3 text-gray-600 hs-accordion-active:hidden hs-accordion-active:text-blue-600 hs-accordion-active:group-hover:text-blue-600 group-hover:text-gray-500 dark:text-gray-400"
+            width="16"
+            height="16"
+            viewBox="0 0 16 16"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+        >
+            <path
+            d="M2.62421 7.86L13.6242 7.85999"
+            stroke="currentColor"
+            strokeWidth="2"
+            strokeLinecap="round"
+            />
+            <path
+            d="M8.12421 13.36V2.35999"
+            stroke="currentColor"
+            strokeWidth="2"
+            strokeLinecap="round"
+            />
+        </svg>
+        <svg
+            className="hidden w-3 h-3 text-gray-600 hs-accordion-active:block hs-accordion-active:text-blue-600 hs-accordion-active:group-hover:text-blue-600 group-hover:text-gray-500 dark:text-gray-400"
+            width="16"
+            height="16"
+            viewBox="0 0 16 16"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+        >
+            <path
+            d="M2.62421 7.86L13.6242 7.85999"
+            stroke="currentColor"
+            strokeWidth="2"
+            strokeLinecap="round"
+            />
+        </svg>
+        Why the subject matter? Why tech?
+        </button>
+        <div
+        id="hs-basic-collapse-two"
+        className="hs-accordion-content hidden w-full overflow-hidden transition-[height] duration-300"
+        aria-labelledby="hs-basic-heading-two"
+        >
+        <p className="text-gray-800 dark:text-gray-200">
+        Well I'm a developer, so that's something. But also, is there a more absurd industry than tech? 
+        </p>
         </div>
-  
-        <div className="hs-accordion" id="hs-basic-heading-three">
-          <button
-            className="inline-flex items-center w-full py-3 font-semibold text-left text-gray-800 transition hs-accordion-toggle hs-accordion-active:text-blue-600 group gap-x-3 hover:text-gray-500 dark:hs-accordion-active:text-blue-500 dark:text-gray-200 dark:hover:text-gray-400"
-            aria-controls="hs-basic-collapse-three"
-          >
-            <svg
-              className="block w-3 h-3 text-gray-600 hs-accordion-active:hidden hs-accordion-active:text-blue-600 hs-accordion-active:group-hover:text-blue-600 group-hover:text-gray-500 dark:text-gray-400"
-              width="16"
-              height="16"
-              viewBox="0 0 16 16"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                d="M2.62421 7.86L13.6242 7.85999"
-                stroke="currentColor"
-                strokeWidth="2"
-                strokeLinecap="round"
-              />
-              <path
-                d="M8.12421 13.36V2.35999"
-                stroke="currentColor"
-                strokeWidth="2"
-                strokeLinecap="round"
-              />
-            </svg>
-            <svg
-              className="hidden w-3 h-3 text-gray-600 hs-accordion-active:block hs-accordion-active:text-blue-600 hs-accordion-active:group-hover:text-blue-600 group-hover:text-gray-500 dark:text-gray-400"
-              width="16"
-              height="16"
-              viewBox="0 0 16 16"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                d="M2.62421 7.86L13.6242 7.85999"
-                stroke="currentColor"
-                strokeWidth="2"
-                strokeLinecap="round"
-              />
-            </svg>
-            Accordion #3
-          </button>
-          <div
-            id="hs-basic-collapse-three"
-            className="hs-accordion-content hidden w-full overflow-hidden transition-[height] duration-300"
-            aria-labelledby="hs-basic-heading-three"
-          >
-            <p className="text-gray-800 dark:text-gray-200">
-              <em>This is the third item's accordion body.</em> It is hidden by
-              default, until the collapse plugin adds the appropriate classes that
-              we use to style each element. These classes control the overall
-              appearance, as well as the showing and hiding via CSS transitions.
-            </p>
-          </div>
+    </div>
+
+    <div className="hs-accordion" id="hs-basic-heading-three">
+        <button
+        className="inline-flex items-center w-full py-3 font-semibold text-left text-gray-800 transition hs-accordion-toggle hs-accordion-active:text-blue-600 group gap-x-3 hover:text-gray-500 dark:hs-accordion-active:text-blue-500 dark:text-gray-200 dark:hover:text-gray-400"
+        aria-controls="hs-basic-collapse-three"
+        >
+        <svg
+            className="block w-3 h-3 text-gray-600 hs-accordion-active:hidden hs-accordion-active:text-blue-600 hs-accordion-active:group-hover:text-blue-600 group-hover:text-gray-500 dark:text-gray-400"
+            width="16"
+            height="16"
+            viewBox="0 0 16 16"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+        >
+            <path
+            d="M2.62421 7.86L13.6242 7.85999"
+            stroke="currentColor"
+            strokeWidth="2"
+            strokeLinecap="round"
+            />
+            <path
+            d="M8.12421 13.36V2.35999"
+            stroke="currentColor"
+            strokeWidth="2"
+            strokeLinecap="round"
+            />
+        </svg>
+        <svg
+            className="hidden w-3 h-3 text-gray-600 hs-accordion-active:block hs-accordion-active:text-blue-600 hs-accordion-active:group-hover:text-blue-600 group-hover:text-gray-500 dark:text-gray-400"
+            width="16"
+            height="16"
+            viewBox="0 0 16 16"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+        >
+            <path
+            d="M2.62421 7.86L13.6242 7.85999"
+            stroke="currentColor"
+            strokeWidth="2"
+            strokeLinecap="round"
+            />
+        </svg>
+        Is there a future for Satirical Silicon?
+        </button>
+        <div
+        id="hs-basic-collapse-three"
+        className="hs-accordion-content hidden w-full overflow-hidden transition-[height] duration-300"
+        aria-labelledby="hs-basic-heading-three"
+        >
+        <p className="text-gray-800 dark:text-gray-200">
+        Heck yeah! I mean, I hope so. I'm having a lot of fun making it, and I hope you're having fun reading it. Maybe one day I'll even make some money off of it, but I'm not holding my breath.
+        </p>
         </div>
-      </div>
+    </div>
+    <div className="hs-accordion" id="hs-basic-heading-three">
+        <button
+        className="inline-flex items-center w-full py-3 font-semibold text-left text-gray-800 transition hs-accordion-toggle hs-accordion-active:text-blue-600 group gap-x-3 hover:text-gray-500 dark:hs-accordion-active:text-blue-500 dark:text-gray-200 dark:hover:text-gray-400"
+        aria-controls="hs-basic-collapse-three"
+        >
+        <svg
+            className="block w-3 h-3 text-gray-600 hs-accordion-active:hidden hs-accordion-active:text-blue-600 hs-accordion-active:group-hover:text-blue-600 group-hover:text-gray-500 dark:text-gray-400"
+            width="16"
+            height="16"
+            viewBox="0 0 16 16"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+        >
+            <path
+            d="M2.62421 7.86L13.6242 7.85999"
+            stroke="currentColor"
+            strokeWidth="2"
+            strokeLinecap="round"
+            />
+            <path
+            d="M8.12421 13.36V2.35999"
+            stroke="currentColor"
+            strokeWidth="2"
+            strokeLinecap="round"
+            />
+        </svg>
+        <svg
+            className="hidden w-3 h-3 text-gray-600 hs-accordion-active:block hs-accordion-active:text-blue-600 hs-accordion-active:group-hover:text-blue-600 group-hover:text-gray-500 dark:text-gray-400"
+            width="16"
+            height="16"
+            viewBox="0 0 16 16"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+        >
+            <path
+            d="M2.62421 7.86L13.6242 7.85999"
+            stroke="currentColor"
+            strokeWidth="2"
+            strokeLinecap="round"
+            />
+        </svg>
+        I wanna help! How can I contribute?
+        </button>
+        <div
+        id="hs-basic-collapse-three"
+        className="hs-accordion-content hidden w-full overflow-hidden transition-[height] duration-300"
+        aria-labelledby="hs-basic-heading-three"
+        >
+        <p className="text-gray-800 dark:text-gray-200">
+        I'm glad you asked! The code is open sourced on <a className="anc" href="https://github.com/tyfiero/SatiricalSilicon" target="_blank" >Github</a> , feel free to use it however you'd like, or submit a pull request if you want to help out with this site. Hit me up on <a className="anc" href="https://twitter.com/FieroTy" target="_blank" >Twitter</a> if you wanna collaborate and make something together.
+        </p>
+        </div>
+    </div>
+    </div>
     );
-  }
-  
+    }
