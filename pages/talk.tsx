7b8173e021b89013e6a64da9695f7194d311f69a
Ty Fiero <tyfierodev@gmail.com>
2023-06-06 16:12:16 -0700

    More changes before talk. Geez

--- pages/talk.tsx
+++ pages/talk.tsx
@@ -17,141 +17,144 @@ function Talk() {
     <>
       <Head>
         <title>Talk</title>
-        <meta
-          name="description"
-          content={`My talk for AI tinkerers`}
-        />
+        <meta name="description" content={`My talk for AI tinkerers`} />
       </Head>
       <div className="flex flex-col items-center page-container !px-0 !mb-0">
-        
-
         <div className="w-full h-[120vh] px-20 py-20 ring-2 bg-gradient-to-br from-white to-p-300 flex flex-col justify-between">
           <MotionHeader>
-          {titleClicked ?   <m.h1
-                    initial={{ letterSpacing: "-0.1em" }}
-                    animate={{ letterSpacing: "0em" }}
-                    className="pt-5 pr-4 font-bold leading-tight tracking-tighter text-transparent text-7xl bg-clip-text bg-gradient-to-br from-p-400 via-p-600 to-s-500 f1 drop-shadow-lg drop-shadow-p-800 h-fit fade-effect-quick"
-                    onClick={() => {
-                      setTitleClicked(!titleClicked);
-                    }
+            {titleClicked ? (
+              <m.h1
+                initial={{ letterSpacing: "-0.1em" }}
+                animate={{ letterSpacing: "0em" }}
+                className="pt-5 pr-4 font-bold leading-tight tracking-tighter text-transparent text-7xl bg-clip-text bg-gradient-to-br from-p-400 via-p-600 to-s-500 f1 drop-shadow-lg drop-shadow-p-800 h-fit fade-effect-quick"
+                onClick={() => {
+                  setTitleClicked(!titleClicked);
+                }}
+              >
+                I Made a GPT-4 App that Pumps Out Satirical Fake News Articles
+                that are Kinda Funny, Automatically!
+              </m.h1>
+            ) : (
+              <m.h1
+                initial={{ letterSpacing: "-0.1em" }}
+                animate={{ letterSpacing: "0em" }}
+                className="pt-5 pr-4 font-bold leading-tight tracking-tighter text-transparent text-7xl bg-clip-text bg-gradient-to-br from-p-400 via-p-600 to-s-500 f1 drop-shadow-lg drop-shadow-p-800 h-fit"
+                onClick={() => {
+                  if (!titleClicked) {
+                    setTitleClicked(true);
                   }
-                  >
-                  I Made a GPT-4 App that Pumps Out Satirical Fake News Articles that are Kinda Funny, Automatically!
-                  </m.h1>:
-                  <m.h1
-                    initial={{ letterSpacing: "-0.1em" }}
-                    animate={{ letterSpacing: "0em" }}
-                    className="pt-5 pr-4 font-bold leading-tight tracking-tighter text-transparent text-7xl bg-clip-text bg-gradient-to-br from-p-400 via-p-600 to-s-500 f1 drop-shadow-lg drop-shadow-p-800 h-fit"
-                    onClick={() => {
-                      if (!titleClicked) {
-                        setTitleClicked(true);
-                      }
-                    }
-                  }
-                  >
-                  Combating Fake News with AI 
-                  </m.h1>}
-                
-                <div className="flex items-center gap-5 mt-4">
-        <img src="/ty.webp" alt="tyfiero" className="w-12 h-12" />
-        <h1 className="text-left heading-lg !text-4xl fade-effect-quick !mb-0 text-gray-800 ">
-                  Ty Fiero
-                </h1>
-        </div>
-                </MotionHeader>
-              <button className="button-1 w-fit"
-              onClick={() => {
-                if (!buttonClicked) {
-                  setButtonClicked(true);
-                }
-                fetch("/api/cron")
-                  .then((res) => res.json())
-                  .then((res) => console.log(res));
-                }
+                }}
+              >
+                Combating Fake News with AI
+              </m.h1>
+            )}
+
+            <div className="flex items-center gap-5 mt-4">
+              <img src="/ty.webp" alt="tyfiero" className="w-12 h-12" />
+              <h1 className="text-left heading-lg !text-4xl fade-effect-quick !mb-0 text-gray-800 ">
+                Ty Fiero
+              </h1>
+            </div>
+          </MotionHeader>
+          <button
+            className="button-1 w-fit"
+            onClick={() => {
+              if (!buttonClicked) {
+                setButtonClicked(true);
               }
-              >{buttonClicked ? "Generating..." : "Make a post!"}</button>
-
-          
+              fetch("/api/cron")
+                .then((res) => res.json())
+                .then((res) => console.log(res));
+            }}
+          >
+            {buttonClicked ? "Generating..." : "Make a post!"}
+          </button>
         </div>
 
-
         <div className="w-full h-[120vh] px-20 py-20 ring-2 bg-gradient-to-br from-white to-p-50">
-        
-        <h1 className="text-left heading-lg !text-5xl fade-effect-quick !mb-0 ">
-          But Like, Why?
-        </h1>
-        <ol className="text-3xl leading-[4rem] f1 list-disc">
-            <li><strong>Writing and publishing blog posts is tedious</strong> - Why not automate all of it?</li>
-            <li> <strong>Learning</strong> - I needed an excuse to learn Langchain, and be one of those 'cool' AI devs</li>
-            <li><strong>I love comedy</strong> - Especially satirical news like "The Onion"</li>
-            <p className="ml-4 text-2xl"> - Turns out GPT-4 is kinda good at satire <Link href={"/posts"}>👀</Link></p>
-        </ol>
-
-          
+          <h1 className="text-left heading-lg !text-5xl fade-effect-quick !mb-0 ">
+            But Like, Why?
+          </h1>
+          <ol className="text-3xl leading-[4rem] f1 list-disc">
+            <li>
+              <strong>Writing and publishing blog posts is tedious</strong> -
+              Why not automate all of it?
+            </li>
+            <li>
+              {" "}
+              <strong>Learning</strong> - I needed an excuse to learn Langchain,
+              and be one of those 'cool' AI devs
+            </li>
+            <li>
+              <strong>I love comedy</strong> - Especially satirical news like
+              "The Onion"
+            </li>
+            <p className="ml-4 text-2xl">
+              {" "}
+              - Turns out GPT-4 is kinda good at satire{" "}
+              <Link href={"/posts"}>👀</Link>
+            </p>
+          </ol>
         </div>
         <div className="w-full h-[120vh] px-20 py-20 ring-2 bg-gradient-to-br from-white to-p-50">
-        
-        <h1 className="text-left heading-lg !text-5xl fade-effect-quick !mb-0 ">
-          Tech
-        </h1>
-        <img src="/diagram.png" alt="" />
-
-          
+          <h1 className="text-left heading-lg !text-5xl fade-effect-quick !mb-0 ">
+            Tech
+          </h1>
+          <img src="/diagram.png" alt="" />
         </div>
         <div className="w-full h-[120vh] px-20 py-20 ring-2 bg-gradient-to-br from-white to-p-50">
-        
-        <h1 className="text-left heading-lg !text-5xl fade-effect-quick !mb-0 ">
-          What's Next?
-        </h1>
-        <ol className="text-3xl leading-[4rem] f1 list-disc">
+          <h1 className="text-left heading-lg !text-5xl fade-effect-quick !mb-0 ">
+            What's Next?
+          </h1>
+          <ol className="text-3xl leading-[4rem] f1 list-disc">
             <li>Newsletter</li>
+            <li>Twitter Bot</li>
             <li>Midjourney API</li>
             <li>Vercel Cron Jobs to fully automate</li>
+            <li>Better error handling</li>
             <li>Different Subjects</li>
-        </ol>
-
-          
+          </ol>
         </div>
         <div className="w-full h-[120vh] px-20 py-20 ring-2 bg-gradient-to-br from-white to-p-300">
-        <m.h1
-                    initial={{ letterSpacing: "-0.1em" }}
-                    animate={{ letterSpacing: "0em" }}
-                    className="pt-5 pr-4 font-bold leading-tight tracking-tighter text-center text-transparent text-7xl bg-clip-text bg-gradient-to-br from-p-400 via-p-600 to-p-700 f1 drop-shadow-lg drop-shadow-p-800 h-fit"
-                  >
-                    Have ideas? Hit me up!
-                  </m.h1>
-        <div className="flex flex-col w-1/2 gap-10 mx-auto mt-20 h-fit">
-       <div className="flex gap-2 mb-10">
-        <img src="/ty.webp" alt="tyfiero" className="w-20 h-20" />
-        <h1 className="text-left heading-lg !text-5xl fade-effect-quick !mb-0 ">
-          Ty Fiero 
-        </h1>
-        </div>
-       <div className="flex gap-2">
-        <FaTwitter className="text-5xl text-p-400"/>
-          <h1 className="text-left heading-lg !text-3xl fade-effect-quick !mb-0 ">
-            @FieroTy
-          </h1>
-       </div>
-       <div className="flex gap-2">
-        <FaGithub className="text-5xl "/>
-        <h1 className="text-left heading-lg !text-black !text-3xl fade-effect-quick !mb-0 ">
-          tyfiero
-        </h1>
-        </div>
-       <div className="flex gap-2">
-        <FaLinkedin className="text-5xl text-blue-600 "/>
-        <h1 className="text-left heading-lg !text-blue-800 !text-3xl fade-effect-quick !mb-0 ">
-          ty-fiero
-        </h1>
-        </div>
-       <div className="flex gap-2">
-        <FaGlobe className="text-5xl "/>
-        <h1 className="text-left heading-lg  !text-3xl fade-effect-quick !mb-0 ">
-          https://satiricalsilicon.com
-        </h1>
-        </div>
-        </div>
+          <m.h1
+            initial={{ letterSpacing: "-0.1em" }}
+            animate={{ letterSpacing: "0em" }}
+            className="pt-5 pr-4 font-bold leading-tight tracking-tighter text-center text-transparent text-7xl bg-clip-text bg-gradient-to-br from-p-400 via-p-600 to-p-700 f1 drop-shadow-lg drop-shadow-p-800 h-fit"
+          >
+            Have ideas? Hit me up!
+          </m.h1>
+          <div className="flex flex-col w-1/2 gap-10 mx-auto mt-20 h-fit">
+            <div className="flex gap-2 mb-10">
+              <img src="/ty.webp" alt="tyfiero" className="w-20 h-20" />
+              <h1 className="text-left heading-lg !text-5xl fade-effect-quick !mb-0 ">
+                Ty Fiero
+              </h1>
+            </div>
+            <div className="flex gap-2">
+              <FaTwitter className="text-5xl text-p-400" />
+              <h1 className="text-left heading-lg !text-3xl fade-effect-quick !mb-0 ">
+                @FieroTy
+              </h1>
+            </div>
+            <div className="flex gap-2">
+              <FaGithub className="text-5xl " />
+              <h1 className="text-left heading-lg !text-black !text-3xl fade-effect-quick !mb-0 ">
+                tyfiero
+              </h1>
+            </div>
+            <div className="flex gap-2">
+              <FaLinkedin className="text-5xl text-blue-600 " />
+              <h1 className="text-left heading-lg !text-blue-800 !text-3xl fade-effect-quick !mb-0 ">
+                ty-fiero
+              </h1>
+            </div>
+            <div className="flex gap-2">
+              <FaGlobe className="text-5xl " />
+              <h1 className="text-left heading-lg  !text-3xl fade-effect-quick !mb-0 ">
+                https://satiricalsilicon.com
+              </h1>
+            </div>
+          </div>
         </div>
       </div>
     </>
